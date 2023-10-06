6d1f0f850afabed2ac08e207ff1b15de6cdaeaac
Eriklps <erikdslopes@gmail.com>
2023-10-06 17:46:40 -0300

    orders edit

--- script/orders.js
+++ script/orders.js
@@ -1,44 +1,44 @@
 const Orders = [ 
     {
-        productName: 'Foldable Mini Drone',
+        productName: 'Product-1',
         orderNumber: '85631',
         paymentStatus: 'Approved',
         shipping: 'Pendent'
     },
 
     {
-        productName: 'LARVENDER KF102 Drone',
+        productName: 'Product-2',
         orderNumber: '36378',
         paymentStatus: 'Refund',
         shipping: 'Canceled'
     },
 
     {
-    productName: 'Ruko F11 Pro Drone',
-    orderNumber: '49378',
-    paymentStatus: 'Approved',
-    shipping: 'Pendent'
+        productName: 'Product-3',
+        orderNumber: '49378',
+        paymentStatus: 'Approved',
+        shipping: 'Pendent'
     },
 
     {
-        productName: 'Drone with Camera',
+        productName: 'Product-4',
         orderNumber: '96968',
         paymentStatus: 'Approved',
         shipping: 'Delivered'
     },
 
     {
-    productName: 'GPS 4k Drone',
-    orderNumber: '22789',
-    paymentStatus: 'Approved',
-    shipping: 'Delivered'
+        productName: 'Product-5',
+        orderNumber: '22789',
+        paymentStatus: 'Approved',
+        shipping: 'Delivered'
     },
 
     {
-    productName: 'Lozenger Drone',
-    orderNumber: '00488',
-    paymentStatus: 'Refund',
-    shipping: 'Canceled'
+        productName: 'Product-6',
+        orderNumber: '00488',
+        paymentStatus: 'Refund',
+        shipping: 'Canceled'
     },
 
 ]
